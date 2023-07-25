4905075535b6cc56d1896c560e651efab538777a
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2023-07-24 20:13:33 -0400

    TS and Node installed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const PORT = 3000
 
 app.get('/', (req, res) => {
   res.send('iPlayed!')
 })
 
-app.listen(port, () => {
-  console.log(`App backend listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`App backend listening on port ${PORT}`)
 })
