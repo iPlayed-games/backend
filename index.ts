8cf23a2a27e9432b76782955a6bf0d0fdb087591
DanielMJSilva <danielmjs609@gmail.com>
2023-07-26 12:17:40 -0400

    fix: change server connection to TS

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('IPlayed!');
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
