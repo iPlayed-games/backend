bdb9f42afc95d7e7bb940ccaa08ec53a788ab98b
Raphael <rmc3408@protonmail.com>
2023-07-27 19:02:04 -0400

    fix: aadd types return function

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response): void => {
   res.send('IPlayed!');
 });
 
--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,12 @@ const PORT: string = process.env.PORT!
 
 export async function startServer(): Promise<void> {
   // Database connection
-  await mongoose.connection.on('connected', () => {
+  await mongoose.connection.on('connected', (): void => {
     console.log(`⚡️[database]: Mongo is running!`)
   })
 
   // App server connection
-  await app.listen(PORT, () => {
+  await app.listen(PORT, (): void => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
   })
 }
