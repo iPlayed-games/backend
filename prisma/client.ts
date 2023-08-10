2188fcd232493c0f9c16230f070ef852b7eb938c
Raphael <rmc3408@protonmail.com>
2023-08-10 07:15:36 -0400

    refactor: database update

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,41 +1,46 @@
 import { PrismaClient } from '@prisma/client'
-import { mockData } from "./mock-data"
+// import { mockData } from './mock-data'
 const prisma = new PrismaClient()
 
-async function main() { 
-    try {
-
-        await prisma.user.deleteMany();
-        await prisma.location.deleteMany();
-        await prisma.venue.deleteMany();
-        await prisma.activity.deleteMany();
-
-        const locationCreate = await prisma.location.create({
-           data: mockData.location.data1
-         })
-         console.log(locationCreate)
-
-        const venueCreate = await prisma.venue.create({
-            data: mockData.venue.data1
-        })
-        console.log(venueCreate)
-
-        const userCreate = await prisma.user.create({
-            data: mockData.user.data1
-        })
-        console.log(userCreate)
-
-        const activityCreate = await prisma.activity.create({
-            data: mockData.activity.data1
-        })
-        console.log(activityCreate)
-
-    } catch(e) {
-        console.error(e)
-    } finally {
-        await prisma.$disconnect()
-    }
- 
+// TODO: for testing purpose only
+async function main() {
+  try {
+    // Delete methods
+    // await prisma.user.deleteMany();
+    // await prisma.location.deleteMany();
+    // await prisma.venue.deleteMany();
+    // await prisma.activity.deleteMany();
+    // Create Methods
+    // const locationCreate = await prisma.location.create({
+    //   data: mockData.location.data1,
+    // })
+    // console.log(locationCreate)
+    // const venueCreate = await prisma.venue.create({
+    //   data: mockData.venue.data1,
+    // })
+    // console.log(venueCreate)
+    // const activityCreate = await prisma.activity.create({
+    //   data: mockData.activity.data1,
+    // })
+    // console.log(activityCreate)
+    // Find Users
+    // const userInfo = await prisma.user.findFirst({
+    //   include: {
+    //     venue: {
+    //       include: {
+    //         location: true,
+    //         Activity: true,
+    //       },
+    //     },
+    //   },
+    // })
+    // console.log(userInfo)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    // TODO: add to server method
+    await prisma.$disconnect()
+  }
 }
 
 main()
