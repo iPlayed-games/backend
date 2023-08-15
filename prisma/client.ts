d4da018ec9337fc9dcb4b579c881dc098bd35eb7
DanielMJSilva <danielmjs609@gmail.com>
2023-08-15 14:55:08 -0400

    Fix: update mock data

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,28 +1,28 @@
 import { PrismaClient } from '@prisma/client'
-// import { mockData } from './mock-data'
+import { data } from './mock-data'
 const prisma = new PrismaClient()
 
 // TODO: for testing purpose only
 async function main() {
   try {
     // Delete methods
-    // await prisma.user.deleteMany();
-    // await prisma.location.deleteMany();
-    // await prisma.venue.deleteMany();
-    // await prisma.activity.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.location.deleteMany();
+    await prisma.venue.deleteMany();
+    await prisma.activity.deleteMany();
     // Create Methods
-    // const locationCreate = await prisma.location.create({
-    //   data: mockData.location.data1,
-    // })
-    // console.log(locationCreate)
-    // const venueCreate = await prisma.venue.create({
-    //   data: mockData.venue.data1,
-    // })
-    // console.log(venueCreate)
-    // const activityCreate = await prisma.activity.create({
-    //   data: mockData.activity.data1,
-    // })
-    // console.log(activityCreate)
+    const locationCreate = await prisma.location.create({
+      data: data.user
+    })
+    console.log(locationCreate)
+    const venueCreate = await prisma.venue.create({
+      data: data.venue
+    })
+    console.log(venueCreate)
+    const activityCreate = await prisma.activity.create({
+      data: data.activity
+    })
+    console.log(activityCreate)
     // Find Users
     // const userInfo = await prisma.user.findFirst({
     //   include: {
--- prisma/mock-data.ts
+++ prisma/mock-data.ts
@@ -1,93 +1,48 @@
-import { Category, DifficultyRating } from '@prisma/client'
+import { Category } from '@prisma/client'
 
 //FIXME: add typescript types, change structure to array objects, export individual
-const location = {
-  data1: {
-    address: '1 Yonge Street',
-    phone: '4575446666',
-    city: 'Toronto',
-    postalCode: 'M4P 020',
-    latitude: 41,
-    longitude: -38,
-  },
-  data2: {
-    address: '12 Mount Plaesant Rd',
-    phone: '6475556969',
-    city: 'Toronto',
-    postalCode: 'M2P 120',
-    latitude: 42,
-    longitude: -37,
-  },
-}
 
-const user = {
-  data1: {
-    //id: userId1,
-    name: 'Daniel',
-    email: 'danielmjs609@gmail.com',
-    password: '12345',
-    wishList: ['1', '2'],
-  },
-  data2: {
-    //id: userId2,
-    name: 'Raphael',
-    email: 'raphael@gmail.com',
-    password: '54321',
-    wishList: ['2', '3'],
-  },
+export const data = {
+    location: {
+        address: '1 Yonge Street',
+        phone: '4575446666',
+        city: 'Toronto',
+        postalCode: 'M4P 020',
+        latitude: 41,
+        longitude: -38,
+    },
+    user: {
+        name: 'Daniel',
+        email: 'danielmjs609@gmail.com',
+        password: '12345',
+        username: 'danielmjs',
+        totalBadge: 5
+    },
+    venue: {
+        name: 'Play Center',
+        email: 'playcenter@gmail.com',
+        description: 'Write some descrition here',
+        activity: ['66d294917ff44d30b4b08d9d'],
+        location: "11d294917ff44d30b4b08d9d",
+        logo: 'http://logo.iplayed.com',
+        mainPhoto: 'http://mainphoto.iplayed.com',
+        isAcessibility: false,
+        workingHoursOpen: 1692349200000, //9am
+        workingHoursClose: 1692381600000, //6pm
+        userId: '45d294917ff44d30b4b08d9d',
+    },
+    activity: {
+        name: 'play game 1',
+        description: 'write something about the activity',
+        image: 'http://www.playgame.com/image1',
+        venueId: "45d294917ff44d30b4b08d9d",
+        startsTies: [1692349200000, 1692381600000],
+        duration: 45,
+        category: <Category>'Indoor',
+        minPlayers: 2,
+        maxPlayers: 10,
+        ageRestriction: 18,
+        location: '13d294917ff44d30b4b08d9d',
+        price: 50.00,
+    }
 }
-
-const venue = {
-  data1: {
-    name: 'Play Center',
-    email: 'playcenter@gmail.com',
-    description: 'Write some descrition here',
-    reviews: 'Write some reviews here',
-    logo: 'http://logo.iplayed.com',
-    mainPhoto: 'http://mainphoto.iplayed.com',
-    bannerPhoto: 'http://bannerphoto.iplayed.com',
-    activityId: '66d294917ff44d30b4b08d9d',
-    userId: '45d294917ff44d30b4b08d9d',
-  },
-  data2: {
-    name: 'Game Show',
-    email: 'gameshow@gmail.com',
-    description: 'Write some descrition here',
-    reviews: 'Write some reviews here',
-    logo: 'http://logo.gameshow.com',
-    mainPhoto: 'http://mainphoto.gameshow.com',
-    bannerPhoto: 'http://bannerphoto.gameshow.com',
-    activityId: '88d294917ff44d30b4b08d9d',
-    userId: '35d294917ff44d30b4b08d9d',
-  },
-}
-
-const activity = {
-  data1: {
-    name: 'play game 1',
-    description: 'write something about the activity',
-    image: 'http://www.playgame.com/image1',
-    duration: 45,
-    category: <Category>'Indoor',
-    numPlayers: 7,
-    minPlayers: 2,
-    maxPlayers: 10,
-    difficultRating: <DifficultyRating>'Normal',
-    likeReview: 4.8,
-    userId: '22d38768d6ddcf97edae0a1a',
-  },
-  data2: {
-    name: 'play Shop',
-    description: 'write something about the activity',
-    image: 'http://www.gameshop.com/image1',
-    duration: 60,
-    category: <Category>'Outdoor',
-    numPlayers: 5,
-    minPlayers: 4,
-    maxPlayers: 12,
-    difficultRating: <DifficultyRating>'Bad',
-    likeReview: 3.8,
-  },
-}
-
-export const mockData = { location, venue, activity, user }
