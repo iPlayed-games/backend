1d8df8ee5ee65ab69fa609249a5cbe7ab78c36cb
DanielMJSilva <danielmjs609@gmail.com>
2023-08-29 09:58:59 -0400

    fix: tsconfig path created

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from 'express'
-import * as UserService from '../services/userService'
+import * as UserService from 'services/userService'
 
 export const getAllUsers = async (_req: Request, res: Response): Promise<void> => {
   const allUser = await UserService.getAllUsers()
