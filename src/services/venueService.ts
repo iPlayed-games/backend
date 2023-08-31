cbf17fcc5d0371a9b6e4271c00095b7bdb52d1be
John Boi <jon0908776@gmail.com>
2023-08-31 13:29:41 -0400

    53-GetVenueById-error-handling

--- src/services/venueService.ts
+++ src/services/venueService.ts
@@ -1,16 +1,25 @@
 import { type Venue } from '@prisma/client'
 import prisma from '../config/prisma/prisma-client'
 import { AllIncludeActivity } from '../queries/queries'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 export async function getAllVenues(): Promise<Venue[]> {
   return await prisma.venue.findMany(AllIncludeActivity)
 }
 
-export async function getVenueById(venueId: string): Promise<Venue> {
-  return await prisma.venue.findUniqueOrThrow({
-    where: {
-      id: venueId,
-    },
-    ...AllIncludeActivity,
-  })
+export async function getVenueById(venueId: string): Promise<Venue | {}> {
+  try {
+    return await prisma.venue.findUniqueOrThrow({
+      where: {
+        id: venueId,
+      },
+      ...AllIncludeActivity,
+    })
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      return { realErrorMessage: err, message: 'Cannot find venue with id: ' + venueId }
+    } else {
+      return { realErrorMessage: err, message: 'Something has gone wrong...' }
+    }
+  }
 }
